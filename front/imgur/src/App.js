c778168821152b9c6b61148ebd3688338c2e1aee
Jean <jeanlucafp@gmail.com>
2019-02-08 19:34:54 -0200

    esqueci de commita esse carai

--- front/imgur/src/App.js
+++ front/imgur/src/App.js
@@ -8,14 +8,27 @@ import filesize from 'filesize'
 import api from './services/api'
 
 class App extends Component {
-
   state = {
     uploadedFiles: []
+  };
+
+  async componentDidMount() {
+    const response = await api.get("posts");
+
+    this.setState({
+      uploadedFiles: response.data.map(file => ({
+        id: file._id,
+        name: file.name,
+        readableSize: filesize(file.size),
+        preview: file.url,
+        uploaded: true,
+        url: file.url
+      }))
+    });
   }
 
   handleUpload = files => {
-  
-    const _uploadedFiles = files.map(file => ({
+    const uploadedFiles = files.map(file => ({
       file,
       id: uniqueId(),
       name: file.name,
@@ -25,68 +38,78 @@ class App extends Component {
       uploaded: false,
       error: false,
       url: null
-    }))
+    }));
 
     this.setState({
-      uploadedFiles: this.state.uploadedFiles.concat(_uploadedFiles)
-    })
-    console.log(uploadedFiles)
-    _uploadedFiles.forEach(this.processUpload)
-  }
+      uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles)
+    });
+
+    uploadedFiles.forEach(this.processUpload);
+  };
 
   updateFile = (id, data) => {
     this.setState({
-      uploadedFiles: this.state.uploadedFiles.map(file => {
-        return id == file.id ? { ...file, ...data } : file
+      uploadedFiles: this.state.uploadedFiles.map(uploadedFile => {
+        return id === uploadedFile.id
+          ? { ...uploadedFile, ...data }
+          : uploadedFile;
       })
-    })
-  }
+    });
+  };
 
-  processUpload = (file) => {
-    const data = new FormData()
-    data.append('file', file.file, file.name)
-    api.post('posts', data, {
-      onUploadProgress: e => {
-        const progress = parseInt(Math.round(e.loaded * 100 / e.total))
-        this.updateFile(file.id, {
-          progress,
-        })
-      },
-    }).then(response => {
-      console.log(response, `success`)
-      this.updateFile(file.id, {
-        uploaded: true,
-        id: response.data._id,
-        url: response.data.url
+  processUpload = uploadedFile => {
+    const data = new FormData();
+
+    data.append("file", uploadedFile.file, uploadedFile.name);
+
+    api
+      .post("posts", data, {
+        onUploadProgress: e => {
+          const progress = parseInt(Math.round((e.loaded * 100) / e.total));
+
+          this.updateFile(uploadedFile.id, {
+            progress
+          });
+        }
       })
-    }).catch(() => {
-      this.updateFile(file.id, {
-        error: true
+      .then(response => {
+        this.updateFile(uploadedFile.id, {
+          uploaded: true,
+          id: response.data._id,
+          url: response.data.url
+        });
       })
-    })
-  }
+      .catch(() => {
+        this.updateFile(uploadedFile.id, {
+          error: true
+        });
+      });
+  };
 
   handleDelete = async id => {
-    await api.delete(`posts/${id}`)
+    await api.delete(`posts/${id}`);
+
     this.setState({
-      uploadedFiles: this.state.uploadedFiles.filter(file => file.id != id)
-    })
+      uploadedFiles: this.state.uploadedFiles.filter(file => file.id !== id)
+    });
+  };
+
+  componentWillUnmount() {
+    this.state.uploadedFiles.forEach(file => URL.revokeObjectURL(file.preview));
   }
 
   render() {
-    const { uploadedFiles } = this.state
+    const { uploadedFiles } = this.state;
+
     return (
       <Container>
         <Content>
-          <Upload onUpload={this.handleUpload}>
-            {!!uploadedFiles.length && (
-              <FileList files={uploadedFiles}> onDelete={this.handleDelete}
-              </FileList>
-            )}
-          </Upload>
+          <Upload onUpload={this.handleUpload} />
+          {!!uploadedFiles.length && (
+            <FileList files={uploadedFiles} onDelete={this.handleDelete} />
+          )}
         </Content>
-        <GlobalStyle />>
-
+        <GlobalStyle />
       </Container>
     );
   }
