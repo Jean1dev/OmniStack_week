beac43ac51da0244d199a222d41d57e3b51838c7
Jean <jeanlucafp@gmail.com>
2019-10-02 23:54:53 -0300

    backend dia 1

---
+++ week-9/backend/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const routes = require('./routes')
+const mongoose = require('mongoose')
+const port = 3333
+
+const app = express()
+
+mongoose.connect('mongodb+srv://default:default@jaguardb-e7nlh.mongodb.net/default?retryWrites=true&w=majority', 
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(port, () => {
+    console.log(`listem on port`, port)
+})
