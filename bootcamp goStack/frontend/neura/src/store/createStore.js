bc54f8bb063e61666d4e23f6cbfe9d86be87cf64
Jean <jeanlucafp@gmail.com>
2020-01-06 22:22:31 -0300

    ajustes no create-store

--- bootcamp goStack/frontend/neura/src/store/createStore.js
+++ bootcamp goStack/frontend/neura/src/store/createStore.js
@@ -1,9 +1,11 @@
 import { createStore, compose, applyMiddleware } from 'redux'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 export default (reducers, middlewares) => {
     const enhancer = process.env.NODE_ENV === 'devlopment'
-        ? compose(console.tron.createEnhancer(), applyMiddleware(...middlewares))
-        : applyMiddleware( ...middlewares)
+        ? composeEnhancers(compose(console.tron.createEnhancer(), applyMiddleware(...middlewares)))
+        : composeEnhancers(applyMiddleware( ...middlewares))
         
     return createStore(reducers, enhancer)
 }
