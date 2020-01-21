64de2cd6842cd27ee39f7b98038dbcbe083e1f87
Jean <jeanlucafp@gmail.com>
2020-01-20 21:29:04 -0300

    aula 30

--- bootcamp goStack/frontend/neura/src/pages/Dashboard/index.js
+++ bootcamp goStack/frontend/neura/src/pages/Dashboard/index.js
@@ -1,37 +1,54 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import api from '../../services/api'
+import { format, subDays, addDays } from 'date-fns'
+import pt from 'date-fns/locale/pt'
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date())
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM ", { locale: pt }),
+    [date]
+  )
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1))
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1))
+  }
+
   return (
-   <Container>
-     <header>
-       <button type="type">
-         <MdChevronLeft size={36} color="#FFF"/>
-       </button>
+    <Container>
+      <header>
+        <button type="type" onClick={handlePrevDay}>
+          <MdChevronLeft size={36} color="#FFF" />
+        </button>
 
-        <strong> 31 de maio</strong>
+        <strong>{date}</strong>
 
-       <button type="type">
-         <MdChevronRight size={36} color="#FFF"/>
-       </button>
-     </header>
+        <button type="type" onClick={handleNextDay}>
+          <MdChevronRight size={36} color="#FFF" />
+        </button>
+      </header>
 
-     <ul>
-       <Time past>
-         <strong>08:00</strong>
-         <span>Claudete</span>
-       </Time>
-       <Time available>
-         <strong>09:00</strong>
-         <span>Em aberto</span>
-       </Time>
-       <Time available>
-         <strong>10:00</strong>
-         <span>Em aberto</span>
-       </Time>
-     </ul>
-   </Container>
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Claudete</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time available>
+          <strong>10:00</strong>
+          <span>Em aberto</span>
+        </Time>
+      </ul>
+    </Container>
   )
 }
